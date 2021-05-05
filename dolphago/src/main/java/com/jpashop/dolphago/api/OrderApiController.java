0a434a1c635061e0329957b2e20780762c8e4146
DolphaGo <adamdoha@naver.com>
2021-05-06 08:09:35 +0900

    JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- dolphago/src/main/java/com/jpashop/dolphago/api/OrderApiController.java
+++ dolphago/src/main/java/com/jpashop/dolphago/api/OrderApiController.java
@@ -92,6 +92,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Getter
     private class OrderDto {
         private Long orderId;
--- dolphago/src/main/java/com/jpashop/dolphago/repository/order/query/OrderQueryRepository.java
+++ dolphago/src/main/java/com/jpashop/dolphago/repository/order/query/OrderQueryRepository.java
@@ -1,6 +1,8 @@
 package com.jpashop.dolphago.repository.order.query;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
 
@@ -41,5 +43,39 @@ public class OrderQueryRepository {
                 + " from Order o" +
                 " join o.member m" +
                 " join o.delivery d", OrderQueryDto.class).getResultList();
+    }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+        //기존 V4의 단점이 Loop를 도는 것이였다. 이를 한방에 가져와서 개선해보려고 한다.
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = findORderItemMap(toOrderIds(result));
+
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+
+        return result;
+        /**
+         * 무엇이 개선되었는가?
+         * 쿼리를 총 2번만 날리고, 메모리에서 처리를 하는 것임
+         */
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findORderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                "select new com.jpashop.dolphago.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)"
+                + " from OrderItem oi"
+                + " join oi.item i"
+                + " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                                               .setParameter("orderIds", orderIds)
+                                               .getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                                                                    .collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+        return orderItemMap;
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream()
+                     .map(o -> o.getOrderId())
+                     .collect(Collectors.toList());
     }
 }
